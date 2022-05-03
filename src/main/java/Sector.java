458aba70e4f5603b17d28a258af8cc17418af6d3
Maqueda Paul <maquedapablodavid@gmail.com>
2022-05-03 14:59:11 -0300

    minor fix

--- src/main/java/Sector.java
+++ src/main/java/Sector.java
@@ -3,10 +3,9 @@ import static java.util.Objects.requireNonNull;
 
 public class Sector {
   List<Miembro> miembros;
-  public Sector(Organizacion unaOrganizacion, List<Miembro> unosMiembros){
+  public Sector(List<Miembro> unosMiembros){
     requireNonNull(unaOrganizacion);
     //un sector podría estar vacio? si todavía no se asignaron supongo que si
-    organizacionAlaQuePertenece = unaOrganizacion;
     miembros = unosMiembros;
   }
   void admitirMiembro (Miembro unMiembro){

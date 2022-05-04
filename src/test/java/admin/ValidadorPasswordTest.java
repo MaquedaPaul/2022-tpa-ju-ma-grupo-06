1d11ca00ce7ad6fc02426cf0388b2ee5f2712e11
francoparente <fparente14@gmail.com>
2022-05-04 19:59:40 -0300

    Genero tests

--- src/test/java/admin/ValidadorPasswordTest.java
+++ src/test/java/admin/ValidadorPasswordTest.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 
 public class ValidadorPasswordTest {
     @Test
-    void testIsInFile() {
-
-    }
-
-    @Test
     void testValidarPassword() {
 
     }
---
+++ src/test/java/admin/validations/ValidacionMinCaracteresTest.java
@@ -0,0 +1,10 @@
+package admin.validations;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidacionMinCaracteresTest {
+    @Test
+    void testValidar() {
+
+    }
+}

4a37edc2fc3cbff6f85e72474e7711c350bdf88a
francoparente <fparente14@gmail.com>
2022-05-04 15:40:55 -0300

    cambios al validadorPassword

--- src/main/java/admin/ValidadorPassword.java
+++ src/main/java/admin/ValidadorPassword.java
@@ -7,23 +7,34 @@ import java.util.Scanner;
 
 public class ValidadorPassword {
 
-  public static void main(String[] args) throws IOException {
+  public boolean validarPassword(String password) throws IOException {
 
-    System.out.println("\nEnter a password:");
-    Scanner scanner = new Scanner(System.in);
-    String password = scanner.nextLine();
-
-    if (!isInFile(password) && (password.length() >= 8)) {
-      System.out.print("\nThe Password is valid!\n\n");
-    } else {
-      System.out.print("\nThe Password is not valid.\n\n");
+    if (isInFile(password)) {
+      // throw isInFileException("Contraseña vulnerable.");
+      return false;
+    } else if (password.length() < 8) {
+      // throw isTooShort("Contraseña con menos de 8 caracteres.");
+      return false;
     }
-
+    return true;
   }
 
   static boolean isInFile(String password) throws IOException {
-    String path =
-        "/home/fparente/Documents/DDS/2022-tpa-ju-ma-grupo-06/src/main/java/admin/top_10k_worst_passwords.txt";
+    String path = "../2022-tpa-ju-ma-grupo-06/src/main/java/admin/top_10k_worst_passwords.txt";
     return Files.lines(Paths.get(path)).anyMatch(line -> line.contains(password));
   }
 }
+
+
+// public static class isInFileException extends RuntimeException {
+// public isInFileException() {
+// super
+// }
+// }
+
+
+// public static class isTooShortException extends RuntimeException {
+// public isInFileException() {
+// super
+// }
+// }

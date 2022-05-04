934734ff3925b0784488c6cb1314f6879f9b1d6d
francoparente <fparente14@gmail.com>
2022-05-04 15:22:50 -0300

    genero ValidadorPasswordTest

---
+++ src/test/java/admin/ValidadorPasswordTest.java
@@ -0,0 +1,15 @@
+package admin;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidadorPasswordTest {
+    @Test
+    void testIsInFile() {
+
+    }
+
+    @Test
+    void testValidarPassword() {
+
+    }
+}

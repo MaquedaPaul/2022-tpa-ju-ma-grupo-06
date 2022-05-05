3c142d6b9d4f35c9da78734d51aefacd05c52a90
francoparente <fparente14@gmail.com>
2022-05-05 13:24:15 -0300

    genero tests que quedan por hacer

---
+++ src/test/java/admin/AdministradorTest.java
@@ -0,0 +1,15 @@
+package admin;
+
+import org.junit.jupiter.api.Test;
+
+public class AdministradorTest {
+    @Test
+    void testGetPassword() {
+
+    }
+
+    @Test
+    void testGetUsuario() {
+
+    }
+}
---
+++ src/test/java/admin/RegistroAdministradorTest.java
@@ -0,0 +1,10 @@
+package admin;
+
+import org.junit.jupiter.api.Test;
+
+public class RegistroAdministradorTest {
+    @Test
+    void testConstruir() {
+
+    }
+}

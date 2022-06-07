276ffdbfe6af004b7a635096b02368869a2dc03c
pablo <maquedapablodavid@gmail.com>
2022-06-07 18:53:09 -0300

    Ejemplo de tipo de consumo "gas natural"

---
+++ src/main/java/GasNatural.java
@@ -0,0 +1,8 @@
+public class GasNatural extends TipoConsumo{
+  public GasNatural(){
+    unidad = "m3";
+    actividad = TipoActividad.COMBUSTION_FIJA;
+    alcance = TipoAlcance.EMISION_DIRECTA;
+  }
+
+}

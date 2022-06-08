ed430dda427019dd2a46687d9ec028c28d08791a
LukBlan <lublanc@frba.utn.edu.ar>
2022-06-08 15:43:40 -0300

    Agregado Repos + Factores de Emicion

--- src/test/java/admin/AdministradorTest.java
+++ src/test/java/admin/AdministradorTest.java
@@ -2,14 +2,17 @@ package admin;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class AdministradorTest {
 
-    @Test
-    void testGetPassword() {
-
-    }
-
-    @Test
-    void testGetUsuario() {
-    }
+  @Test
+  void agregarUnFactorDeEmision() {
+    Administrador unAdministrador = new Administrador("ejemplo","esteEsUnEjemplo");
+    RepoFactoresEmision unRepo = new RepoFactoresEmision();
+    unAdministrador.setRepoDeFactores(unRepo);
+    unAdministrador.crearFactorEmision(300,"lts");
+    assertEquals(unRepo.getFactoresEmision().size(), 1);
+  }
 }

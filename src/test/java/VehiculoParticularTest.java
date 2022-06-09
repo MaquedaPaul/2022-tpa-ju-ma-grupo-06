bc935a2a58e6588b842d940d66b0030aa6b2cb01
pablo <maquedapablodavid@gmail.com>
2022-06-09 02:09:40 -0300

    Preguntar acerca del mockeo
    Co-authored-by: Thomas Candia <ThomasCandia@users.noreply.github.com>
    Co-authored-by: Lucas Blanco <LukBlan@users.noreply.github.com>
    Co-authored-by: Daniel-Aizcorbe <Daniel-Aizcorbe@users.noreply.github.com>

--- src/test/java/VehiculoParticularTest.java
+++ src/test/java/VehiculoParticularTest.java
@@ -1,29 +1,63 @@
+import linea.Coordenada;
+import linea.PuntoUbicacion;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import transporte.TipoCombustible;
-import transporte.TipoVehiculo;
+import services.geodds.GeoddsService;
+import services.geodds.ServicioGeodds;
+import transporte.*;
 import transporte.VehiculoParticular;
 
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 public class VehiculoParticularTest {
+  Factory unFactory = new Factory();
+  TipoVehiculo esUnAuto = TipoVehiculo.AUTO;
+  TipoCombustible andaANafta = TipoCombustible.NAFTA;
+  VehiculoParticular autoANafta = new VehiculoParticular(TipoCombustible.NAFTA, TipoVehiculo.AUTO);
+////
+  TipoVehiculo esUnaMoto = TipoVehiculo.MOTO;
+  TipoCombustible andaAGasoil = TipoCombustible.GASOIL;
+  VehiculoParticular motoAGasoil = new VehiculoParticular(TipoCombustible.GASOIL, TipoVehiculo.MOTO);
+  ////
 
+  @BeforeEach
+  void init() {
+    GeoddsService geoddsService;
+    geoddsService = mock(GeoddsService.class);
+  }
   @Test
   public void darDeAltaUnAutoANafta() {
-    TipoVehiculo esUnAuto = TipoVehiculo.AUTO;
-    TipoCombustible andaANafta = TipoCombustible.NAFTA;
-    VehiculoParticular autoANafta = new VehiculoParticular(TipoCombustible.NAFTA,TipoVehiculo.AUTO);
-
-    assertEquals(autoANafta.getTipoVehiculo(),esUnAuto);
-    assertEquals(autoANafta.getTipoCombustible(),andaANafta);
+    assertEquals(autoANafta.getTipoVehiculo(), esUnAuto);
+    assertEquals(autoANafta.getTipoCombustible(), andaANafta);
   }
 
   @Test
   public void darDeAltaUnaMotoAGasoil() {
-    TipoVehiculo esUnaMoto = TipoVehiculo.MOTO;
-    TipoCombustible andaAGasoil = TipoCombustible.GASOIL;
-    VehiculoParticular motoAGasoil = new VehiculoParticular(TipoCombustible.GASOIL,TipoVehiculo.MOTO);
-
-    assertEquals(motoAGasoil.getTipoVehiculo(),esUnaMoto);
-    assertEquals(motoAGasoil.getTipoCombustible(),andaAGasoil);
+    assertEquals(motoAGasoil.getTipoVehiculo(), esUnaMoto);
+    assertEquals(motoAGasoil.getTipoCombustible(), andaAGasoil);
   }
+  //////PREGUNTAR
+  /*
+  @Test
+  public void huboInteraccionConLaAPIAlCalcularDistancia() throws IOException {
+    Tramo unTramo = unFactory.crearTramoSimple(motoAGasoil);
+    ServicioGeodds geoddsMock = mock(ServicioGeodds.class);
+    motoAGasoil.servicioGeodds = geoddsMock;
+    //when(geoddsMock.distancia(any(),any(),any(),any(),any(),any())).thenReturn(any());
+    //geoddsMock.distancia(any(),any(),any(),any(),any(),any());
+    //unTramo.distanciaTramo();
+    verify(geoddsMock,times(0)).distancia(anyInt(),anyString(),anyInt(),anyInt(),anyString(),anyInt());
+  }
+  */
+
+        /*
+    int unaAlturaO = unTramo.getPuntoOrigen().getAltura();
+    String unaCalleO = unTramo.getPuntoOrigen().getCalle();
+    int unaLocalidadO = unTramo.getPuntoOrigen().getLocalidadId();
+    int unaAlturaD = unTramo.getPuntoDestino().getAltura();
+    String unaCalleD = unTramo.getPuntoDestino().getCalle();
+    int unaLocalidadD = unTramo.getPuntoDestino().getLocalidadId();
+    */
 }

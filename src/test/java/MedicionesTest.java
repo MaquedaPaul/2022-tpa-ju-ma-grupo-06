ace7b5cf65159266462198202c51fc3f36aa198d
LukBlan <lublanc@frba.utn.edu.ar>
2022-06-09 09:05:36 -0300

    Incorporado Test - noSeEcuentraArchivo

--- src/test/java/MedicionesTest.java
+++ src/test/java/MedicionesTest.java
@@ -1,4 +1,15 @@
+import exceptions.NoExisteElSectorVinculante;
+import exceptions.NoSePudoAbrirElArchivo;
+import mediciones.MedicionBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MedicionesTest {
 
+  @Test
+  public void SeCreaUnaMedicionPeroNoSeEncuentraElArchivo() {
+    assertThrows(NoSePudoAbrirElArchivo.class, () ->  new MedicionBuilder("estaRutaNoExiste.txt"));
 
+  }
 }

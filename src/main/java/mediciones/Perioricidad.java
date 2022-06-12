304cdc76baefd63537a86a72dccc570e7ec7755c
LukBlan <lublanc@frba.utn.edu.ar>
2022-06-12 13:05:21 -0300

    Check styles arreglados - faltan solo bugs

--- src/main/java/mediciones/Perioricidad.java
+++ src/main/java/mediciones/Perioricidad.java
@@ -7,6 +7,7 @@ public enum Perioricidad {
   ANUAL, MENSUAL;
 
   public static boolean esUnPeriodoValido(String periodo) {
-    return Arrays.stream(Perioricidad.values()).map(Enum::toString).anyMatch(unPeriodo -> Objects.equals(unPeriodo, periodo));
+    return Arrays.stream(Perioricidad.values()).map(Enum::toString)
+        .anyMatch(unPeriodo -> Objects.equals(unPeriodo, periodo));
   }
 }

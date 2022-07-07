16e5ad0bc91b662ca0384ecf5a725aa0e0461df5
pablo <maquedapablodavid@gmail.com>
2022-07-06 22:01:03 -0300

    Lombok y calcularHC, campo dias que trabaja getters y calcularHCTotal que depende de calcularHCTrayectos

--- src/main/java/organizacion/Miembro.java
+++ src/main/java/organizacion/Miembro.java
@@ -3,26 +3,32 @@ package organizacion;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
-import transporte.Trayecto;
 
+import transporte.Trayecto;
+import lombok.Getter;
+
+@Getter
 public class Miembro {
   String nombre;
   String apellido;
   TIpoDocumento tipoDocumento;
   int numeroDocumento;
   List<Trayecto> trayectos;
+  int cantidadDeDiasQueTrabaja;
 
   public Miembro(
       String unNombre,
       String unApellido,
       TIpoDocumento unTipoDocumento,
       int unNumeroDocumento,
-      List<Trayecto> unosTrayectos) {
-    nombre = unNombre;
-    apellido = unApellido;
-    tipoDocumento = unTipoDocumento;
-    numeroDocumento = unNumeroDocumento;
-    trayectos = unosTrayectos;
+      List<Trayecto> unosTrayectos,
+      int cantidadDeDiasQueTrabaja) {
+    this.nombre = unNombre;
+    this.apellido = unApellido;
+    this.tipoDocumento = unTipoDocumento;
+    this.numeroDocumento = unNumeroDocumento;
+    this.trayectos = unosTrayectos;
+    this.cantidadDeDiasQueTrabaja = cantidadDeDiasQueTrabaja;
   }
 
   void registrarTrayecto(Trayecto unTrayecto) {
@@ -34,19 +40,14 @@ public class Miembro {
     unaOrganizacion.recibirSolicitud(unaSolicitud);
   }
 
-  public String getApellido() {
-    return apellido;
+
+  public double calcularHCTotal() {
+    return getCantidadDeDiasQueTrabaja() * this.calcularHCTrayectos();
   }
 
-  public String getNombre() {
-    return nombre;
-  }
 
-  public int getNumeroDocumento() {
-    return numeroDocumento;
-  }
-
-  public TIpoDocumento getTipoDocumento() {
-    return tipoDocumento;
+  public double calcularHCTrayectos() {
+    return 0;
+    //return getTrayectos().stream().mapToDouble(unTrayecto -> unTrayecto.calcularHC()).sum();
   }
 }

f3f21cea3a16187dda5e5343efc406cbfbf80e80
pablo <maquedapablodavid@gmail.com>
2022-07-14 01:41:02 -0300

    Merge solved

--- src/main/java/notificaciones/medioNotificacion/MedioNotificador.java
+++ src/main/java/notificaciones/medioNotificacion/MedioNotificador.java
@@ -8,6 +8,8 @@ public interface MedioNotificador {
 
   public void enviarATodos(List<Contacto> contactos);
 
+  public void enviarA(Contacto contacto);
+
   public void setMensajeEnvioStandar(String mensaje);
 
   public String getMensajeEnvioPara(Contacto contacto);

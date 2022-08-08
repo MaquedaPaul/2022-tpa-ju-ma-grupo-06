d2a106f6d632926f104f0b3fb5896b4e3b5a9af8
pablo <maquedapablodavid@gmail.com>
2022-08-08 20:51:24 -0300

    Global Clock para tests

---
+++ src/test/java/notificaciones/GlobalClock.java
@@ -0,0 +1,18 @@
+package notificaciones;
+
+import java.time.Clock;
+
+public class GlobalClock {
+
+  public static Clock clock;
+
+  static {
+    reset();
+  }
+
+  public static void reset() { use(Clock.systemDefaultZone()); }
+
+  public static void use(Clock clock) {
+    GlobalClock.clock = clock;
+  }
+}

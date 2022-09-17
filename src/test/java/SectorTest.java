d0b108b8e47f1ebd415e9ba798a251a0a3d34659
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-09-17 01:06:21 -0300

    add test de Sector

--- src/test/java/SectorTest.java
+++ src/test/java/SectorTest.java
@@ -1,12 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import organizacion.*;
+import transporte.Trayecto;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SectorTest {
+
+  Miembro miembro1 = mock(Miembro.class);
+  Miembro miembro2 = mock(Miembro.class);
+
+  List<Miembro> miembros = new ArrayList<>();
+
+  Trayecto trayecto1 = mock(Trayecto.class);
+  Trayecto trayecto2 = mock(Trayecto.class);
+
+  List<Trayecto> trayectos = new ArrayList<>();
+
+  Sector sector = new Sector("sector prueba", miembros);
+
+  @BeforeEach
+  public void init() {
+    miembros.add(miembro1);
+    miembros.add(miembro2);
+
+    trayectos.add(trayecto1);
+    trayectos.add(trayecto2);
+
+    when(miembro1.getTrayectos()).thenReturn(trayectos);
+    when(miembro2.getTrayectos()).thenReturn(trayectos);
+
+    when(trayecto1.calcularHC()).thenReturn(300.3);
+    when(trayecto2.calcularHC()).thenReturn(200.2);
+  }
 
   @Test
   public void unMiembroSeVinculaConUnSector() {
@@ -25,11 +57,30 @@ class SectorTest {
         size(), 1);
   }
 
+  @Test
+  public void alObtenerTrayectosDeLosMiembrosNoObtengoLosRepetidos() {
+
+    assertEquals((int) sector.getTrayectosDeMiembros().count(), 2);
+
+  }
+
+  @Test
+  public void elHCTotalDeLosMiembrosEsLaSumaDelHCDeSusTrayectosSinRepetidos() {
+
+    assertEquals(sector.calcularHCTotalDeMiembros(), 500.5);
+  }
+
+  @Test
+  public void elHCPromedioPorMiembroEsElTotalSobreLaCantidadDeMiembros() {
+
+    assertEquals(500.5 / 2, sector.calcularPromedioHCPorMiembro());
+
+  }
+
   public Miembro generarMiembro(String nombre,
                                 String apellido,
                                 int documento,
                                 TipoDocumento unTipo) {
-    MiembroTest testMiembro = new MiembroTest();
-    return testMiembro.generarMiembro(nombre, apellido, documento, unTipo);
+    return MiembroTest.generarMiembro(nombre, apellido, documento, unTipo);
   }
 }

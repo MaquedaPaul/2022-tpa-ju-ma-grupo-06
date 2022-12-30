588b5afb8c375a67d98f125701cdb8b71d7d5d41
MaquedaPaul <maquedapablodavid@gmail.com>
2022-12-30 13:56:36 -0300

    Vinculacion fix Ahora solo se muestran las organizaciones a las que no está vinculado el miembro únicamente

--- src/main/java/controllers/miembro/SolicitudVinculacionController.java
+++ src/main/java/controllers/miembro/SolicitudVinculacionController.java
@@ -12,15 +12,17 @@ import spark.Response;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SolicitudVinculacionController {
     public ModelAndView getVinculacion(Request request, Response response) {
-
+        Miembro miembro = MiembroController.obtenerMiembro(request);
         List<Organizacion> organizaciones =  RepoOrganizacion.getInstance().getOrganizaciones();
+        List<Organizacion> organizacionesQueNoPertenece = organizaciones.stream().filter(org -> !org.miembroPerteneceAlaOrganizacion(miembro)).collect(Collectors.toList());
         List<String> nombreSectores = new ArrayList<>(RepoOrganizacion.getInstance().nombreDeTodosLosSectores());
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("nombreSectores",nombreSectores);
-        hashMap.put("organizaciones", organizaciones);
+        hashMap.put("organizaciones", organizacionesQueNoPertenece);
         return new ModelAndView(hashMap,"miembroVinculacion.hbs");
     }
     public ModelAndView pedirVinculacion(Request request, Response response) {

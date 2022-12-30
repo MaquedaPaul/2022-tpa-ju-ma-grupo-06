90e12b05683374bfe8cc87a6cf337ad4549537d2
MaquedaPaul <maquedapablodavid@gmail.com>
2022-12-30 12:30:35 -0300

    Router -> modificaciones necesarias: Para las modificaciones en los controllers

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -1,7 +1,16 @@
 package server;
 
-import controllers.*;
+import controllers.agente.AgenteController;
+import controllers.agente.ComposicionController;
+import controllers.agente.EvolucionController;
+import controllers.agente.OrganizacionesController;
+import controllers.base.*;
+import controllers.miembro.MiembroController;
+import controllers.miembro.SolicitudVinculacionController;
+import controllers.miembro.TrayectosController;
 import controllers.organizacion.*;
+import controllers.base.sign.SignInController;
+import controllers.base.sign.SignOutController;
 import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -18,13 +27,19 @@ public class Router {
     SignOutController signOutController = new SignOutController();
 
     MiembroController miembroController = new MiembroController();
+    TrayectosController trayectosController = new TrayectosController();
+    SolicitudVinculacionController solicitudVinculacionController = new SolicitudVinculacionController();
+
     OrganizacionController organizacionController = new OrganizacionController();
     ImpactoController impactoController = new ImpactoController();
     IndicadorController indicadorController = new IndicadorController();
     MedicionController medicionController = new MedicionController();
     VinculacionController vinculacionController = new VinculacionController();
-    AgenteController agenteController = new AgenteController();
 
+    AgenteController agenteController = new AgenteController();
+    ComposicionController composicionController = new ComposicionController();
+    EvolucionController evolucionController = new EvolucionController();
+    OrganizacionesController organizacionesController = new OrganizacionesController();
     Spark.staticFiles.location("static");
 
 
@@ -55,23 +70,23 @@ public class Router {
     Spark.post("/signout", signOutController::logOut, engine);
 
     //MIEMBRO
-    Spark.get("/home/trayectos", miembroController::getTrayectos, engine);
-    Spark.get("/home/trayectos/registro", miembroController::getRegistrarTrayecto, engine);
-    Spark.get("/home/trayectos/registro/tramo-nuevo", miembroController::getTrayectoNuevo, engine);
-    Spark.post("/home/trayectos/registro/tramo-nuevo", miembroController::cargarTramo, engine);
+    Spark.get("/home/trayectos", trayectosController::getTrayectos, engine);
+    Spark.get("/home/trayectos/registro", trayectosController::getRegistrarTrayecto, engine);
+    Spark.get("/home/trayectos/registro/tramo-nuevo", trayectosController::getTrayectoNuevo, engine);
+    Spark.post("/home/trayectos/registro/tramo-nuevo", trayectosController::cargarTramo, engine);
     Spark.get("/home/trayectos/compartir", miembroController::getRegistro, engine);
     Spark.post("/home/trayectos/compartir", miembroController::getRegistro, engine);
-    Spark.post("/home/trayectos/registro/eliminar", miembroController::eliminarTramo, engine);
-    Spark.post("/home/trayectos/registro/cancelar", miembroController::cancelarTrayecto, engine);
-    Spark.post("/home/trayectos/registro/crear", miembroController::crearTrayecto, engine);
-    Spark.get("/home/vinculacion", miembroController::getVinculacion, engine);
-    Spark.post("/home/vinculacion", miembroController::pedirVinculacion, engine);
+    Spark.post("/home/trayectos/registro/eliminar", trayectosController::eliminarTramo, engine);
+    Spark.post("/home/trayectos/registro/cancelar", trayectosController::cancelarTrayecto, engine);
+    Spark.post("/home/trayectos/registro/crear", trayectosController::crearTrayecto, engine);
+    Spark.get("/home/vinculacion", solicitudVinculacionController::getVinculacion, engine);
+    Spark.post("/home/vinculacion", solicitudVinculacionController::pedirVinculacion, engine);
 
     //ORGANIZACION
     Spark.get("/home/vinculaciones", vinculacionController::getVinculaciones, engine);
     Spark.post("/home/vinculaciones/:id/aceptar", vinculacionController::aceptarVinculacion,engine);
     Spark.post("/home/vinculaciones/:id/rechazar", vinculacionController::rechazarVinculacion,engine);
-    Spark.post("/home/vinculaciones", miembroController::pedirVinculacion, engine);
+    Spark.post("/home/vinculaciones", solicitudVinculacionController::pedirVinculacion, engine);
     Spark.get("/home/mediciones", medicionController::getMediciones, engine);
     Spark.get("/home/mediciones/perse", medicionController::getMedicionesPerse, engine);
     Spark.post("/home/mediciones/perse/creado",medicionController::crearMedicion,engine);
@@ -86,16 +101,16 @@ public class Router {
     Spark.get("/home/calculadora-hc/indicador-hc-sector", indicadorController::getIndicadorHcSector, engine);
 
     //AGENTE
-    Spark.get("/home/composicion-hc",agenteController::getComposicionHc,engine);
-    Spark.get("/home/composicion-hc/grafico",agenteController::getComposicionHcGrafico,engine);
-    Spark.get("/home/evolucion-hc",agenteController::getEvolucionHc,engine);
-    Spark.get("/home/evolucion-hc/grafico",agenteController::getEvolucionHcGrafico,engine);
-    Spark.get("home/organizaciones",agenteController::getOrganizaciones,engine);
+    Spark.get("/home/composicion-hc",composicionController::getComposicionHc,engine);
+    Spark.get("/home/composicion-hc/grafico",composicionController::getComposicionHcGrafico,engine);
+    Spark.get("/home/evolucion-hc",evolucionController::getEvolucionHc,engine);
+    Spark.get("/home/evolucion-hc/grafico",evolucionController::getEvolucionHcGrafico,engine);
+    Spark.get("home/organizaciones",organizacionesController::getOrganizaciones,engine);
     Spark.get("home/hc-total",agenteController::getHcTotal,engine);
-    Spark.get("/home/organizaciones/hc-tipo-organizacion/:tipo",agenteController::getHcTipoOrg,engine);
-    Spark.get("/home/organizaciones/:id/evolucion-hc/consulta",agenteController::getEvolucionHcOrg,engine);
-    Spark.get("/home/organizaciones/:id/evolucion-hc/grafico",agenteController::getEvolucionHcOrgGrafico,engine);
-    Spark.get("/home/organizaciones/:id/composicion-hc/consulta",agenteController::getComposicionHcOrg,engine);
-    Spark.get("/home/organizaciones/:id/composicion-hc/grafico",agenteController::getComposicionHcOrgGrafico,engine);
+    Spark.get("/home/organizaciones/hc-tipo-organizacion/:tipo",organizacionesController::getHcTipoOrg,engine);
+    Spark.get("/home/organizaciones/:id/evolucion-hc/consulta",evolucionController::getEvolucionHcOrg,engine);
+    Spark.get("/home/organizaciones/:id/evolucion-hc/grafico",evolucionController::getEvolucionHcOrgGrafico,engine);
+    Spark.get("/home/organizaciones/:id/composicion-hc/consulta",composicionController::getComposicionHcOrg,engine);
+    Spark.get("/home/organizaciones/:id/composicion-hc/grafico",composicionController::getComposicionHcOrgGrafico,engine);
   }
 }
